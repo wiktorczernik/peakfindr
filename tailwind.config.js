c33e0ce699e1fc3181d9aab301bc634316e563b8
Nickname <88354270+Nicknamick@users.noreply.github.com>
2023-02-06 18:41:26 +0100

    New content

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/**/*.{html,js}',
+    './pages/**/*.{html,php}',
+    './index.html',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      height: {
+        '112': '28rem', //'448px'
+        '128': '32rem', //'512px'
+        '144': '36rem', //'576px'
+        '160': '40rem', //'640px'
+        '176': '44rem', //'704px'
+        '192': '48rem', //'768px'
+      }
+    }
+  }
+}
